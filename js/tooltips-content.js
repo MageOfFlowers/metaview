9c5cce19fe61c4b774e2d4f158449f1f9e2f998c
DUY HỒ KHÁNH <duy.ho@innotech.vn>
2026-04-17 09:40:43 +0700

    l

---
+++ js/tooltips-content.js
@@ -0,0 +1,12 @@
+// js/tooltips-content.js
+export const tooltipData = {
+    "usage_chart": "Biểu đồ này hiển thị tần suất xuất hiện của các lá bài thần theo màu sắc hoặc độ hiếm trong meta hiện tại. 'Theo Deck' tính số bộ bài có chứa thẻ, 'Tổng bản copy' tính dựa trên số lượng thẻ thực tế.",
+    
+    "top_card_chart": "Hiển thị những thần có tỉ lệ thắng và lượt sử dụng cao nhất. Có thể xem theo từng màu.",
+
+    "deck_rank_chart": "Xếp hạng các bộ bài dựa trên Số lượt sử dụng, tỉ lệ thắng và hạng trung bình.",
+    
+    "player_rank_chart": "Bảng vinh danh những người chơi có thành tích ổn định nhất dựa trên tỷ lệ thắng và thứ hạng đạt được qua các giải đấu được chọn.",
+    
+    "meta_list": "Danh sách chi tiết từng thần. Bạn có thể nhấn vào một dòng để xem phân tích sâu hơn."
+};
